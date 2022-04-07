a26556ee326e6441a03645e4f1d2e2846d2084ee
Maksim Svitetskiy <msvitetskiy@bk.ru>
2022-04-08 00:44:14 +0900

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace PLT_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenFileBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.CheckBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tape = new System.Windows.Forms.TextBox();
+            this.StepBtn = new System.Windows.Forms.Button();
+            this.CurStLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenFileBtn
+            // 
+            this.OpenFileBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OpenFileBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
+            this.OpenFileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenFileBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OpenFileBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.OpenFileBtn.Location = new System.Drawing.Point(57, 7);
+            this.OpenFileBtn.Name = "OpenFileBtn";
+            this.OpenFileBtn.Size = new System.Drawing.Size(145, 40);
+            this.OpenFileBtn.TabIndex = 0;
+            this.OpenFileBtn.Text = "OpenFile";
+            this.OpenFileBtn.UseVisualStyleBackColor = false;
+            this.OpenFileBtn.Click += new System.EventHandler(this.OpenFileBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(596, 404);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "(*a{}(***){aa*}{})";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "text files|*.txt";
+            // 
+            // CheckBtn
+            // 
+            this.CheckBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CheckBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CheckBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.CheckBtn.Location = new System.Drawing.Point(57, 53);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(145, 40);
+            this.CheckBtn.TabIndex = 2;
+            this.CheckBtn.Text = "FastCheck";
+            this.CheckBtn.UseVisualStyleBackColor = true;
+            this.CheckBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImage = global::PLT_lab2.Properties.Resources.scheme;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(596, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Panel1MinSize = 175;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(596, 619);
+            this.splitContainer1.SplitterDistance = 211;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Tape);
+            this.panel1.Controls.Add(this.StepBtn);
+            this.panel1.Controls.Add(this.CurStLabel);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.CheckBtn);
+            this.panel1.Controls.Add(this.OpenFileBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(596, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 619);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(3, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "На ленте:";
+            // 
+            // Tape
+            // 
+            this.Tape.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.Tape.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(59)))));
+            this.Tape.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tape.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Tape.Location = new System.Drawing.Point(0, 202);
+            this.Tape.Name = "Tape";
+            this.Tape.ReadOnly = true;
+            this.Tape.Size = new System.Drawing.Size(258, 29);
+            this.Tape.TabIndex = 7;
+            // 
+            // StepBtn
+            // 
+            this.StepBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StepBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StepBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StepBtn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.StepBtn.Location = new System.Drawing.Point(57, 99);
+            this.StepBtn.Name = "StepBtn";
+            this.StepBtn.Size = new System.Drawing.Size(145, 40);
+            this.StepBtn.TabIndex = 6;
+            this.StepBtn.Text = "Step";
+            this.StepBtn.UseVisualStyleBackColor = true;
+            this.StepBtn.Click += new System.EventHandler(this.StepBtn_Click);
+            // 
+            // CurStLabel
+            // 
+            this.CurStLabel.AutoSize = true;
+            this.CurStLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CurStLabel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.CurStLabel.Location = new System.Drawing.Point(150, 151);
+            this.CurStLabel.Name = "CurStLabel";
+            this.CurStLabel.Size = new System.Drawing.Size(0, 25);
+            this.CurStLabel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label2.Location = new System.Drawing.Point(3, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текущее состояние:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 619);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(644, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "CheckChain";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button OpenFileBtn;
+        private TextBox textBox1;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private Button CheckBtn;
+        private PictureBox pictureBox1;
+        private SplitContainer splitContainer1;
+        private Panel panel1;
+        private Label CurStLabel;
+        private Label label2;
+        private Button StepBtn;
+        private Label label1;
+        private TextBox Tape;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿namespace PLT_lab2
+{
+    public partial class Form1 : Form
+    {
+        enum state { H, C1, C, S1, K, S, Err }
+        state cs;
+        state CS
+        {
+            get { return cs; }
+            set { cs = value; CurStLabel.Text = value.ToString(); }
+        }
+        string onTape = "";
+        string OnTape
+        {
+            get { return onTape; }
+            set { onTape = value; Tape.Text = value; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            fileText = System.Text.RegularExpressions.Regex.Replace(fileText, @"\s", "");
+            textBox1.Text = fileText;
+        }
+
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+
+            do
+            {
+                if (OnTape == "")
+                {
+                    if (run == false)
+                    {
+                        CS = state.H;
+                        OnTape = textBox1.Text;
+                        run = true;
+                        return;
+                    }
+                    else
+                    {
+                        OnEmptyTape();
+                    }
+                    return;
+                }
+            }
+            while (MakeStep());
+        }
+
+        bool run = false;
+
+        void OnEmptyTape()//цепочка закончилась
+        {
+            if (CS == state.S)
+                MessageBox.Show( "Цепочка принята","", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            else
+                MessageBox.Show($"Автомат не в конечном состоянии\nПоследнее состояние: {CS}\n", "Цепочка не принята", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            run = false;
+            CurStLabel.Text = "";
+        }
+        private void StepBtn_Click(object sender, EventArgs e)
+        {
+            if (OnTape == "")
+            {
+                if (run == false)
+                {
+                    CS = state.H;
+                    OnTape = textBox1.Text;
+                    run = true;
+                }
+                else
+                {
+                    OnEmptyTape();
+                }
+                return;
+            }
+            MakeStep();
+        }
+        bool MakeStep()
+        {
+            char c = OnTape[0];
+            OnTape = OnTape.Remove(0, 1);
+            switch (CS)
+            {
+                case state.H:
+                    if (c == '(')
+                    {
+                        CS = state.C1;
+                        return true;
+                    }
+                    else if (c == '{')
+                    {
+                        CS = state.K;
+                        return true;
+                    }
+                    else goto default;
+
+                case state.K:
+                    if (c == 'a' || c == '(' || c == '*' || c == ')' || c == '{')
+                    {
+                        return true;
+                    }
+                    else if (c == '}')
+                    {
+                        CS = state.S;
+                        return true;
+                    }
+                    else goto default;
+
+                case state.C1:
+                    if (c == '*')
+                    {
+                        CS = state.C;
+                        return true;
+                    }
+                    else goto default;
+
+                case state.C:
+                    
+                    if (c == '*' && OnTape.LastIndexOf('*')==-1)
+                    {
+                        CS = state.S1;
+                        return true;
+                    }
+                    else if(c == 'a' || c == '{' || c == '}' || c == '(' || c == '*' || c == ')')
+                    {
+                        return true;
+                    }
+                    else goto default;
+
+                case state.S1:
+                    if (c == ')')
+                    {
+                        CS = state.S;
+                        return true;
+                    }
+                    else goto default;
+
+                default:
+                    if (c != 'a' && c != '(' && c != '*' && c != ')' && c != '{' && c != '}') { MessageBox.Show($"символ \"{c}\" не найден", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                    else { MessageBox.Show($"q( {CS} , {c}... )├─q.Error", "ошибочное состояние", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                    run = false;
+                    OnTape = "";
+                    CurStLabel.Text = "";
+                    return false;
+            }
+        }
+    }
+}
